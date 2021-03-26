a7099d3331641e6f9117a59e5703593d4f01182e
Michal Switala <michalswitala@Michals-MacBook-Pro.local>
2021-03-26 15:06:31 +0100

    Adding items

--- lib/main.dart
+++ lib/main.dart
@@ -14,19 +14,52 @@ class ToDoApp extends StatelessWidget{
   }
 }
 
-
 class TodoList extends StatefulWidget{
   @override
   createState()=> new ToDoListState();
 }
 
 class ToDoListState extends State<TodoList>{
+  List<String> _todoItems=[];
+
+  void _addTodoItem(){
+    setState((){
+      int index=_todoItems.length;
+      _todoItems.add('Item'+index.toString());
+    });
+  }
+
+
+  Widget _buildTodoList(){
+    return new ListView.builder(
+      itemBuilder:(context, index){
+        if(index<_todoItems.length){
+          return _buildTodoItem(_todoItems[index]);
+        }
+      },
+    );
+  }
+
+
+
+Widget _buildTodoItem(String todoText) {
+    return new ListTile(
+      title: new Text(todoText)
+    );
+  }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Todo List')
-      )
+      ),
+      body: _buildTodoList(),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _addTodoItem,
+        tooltip: 'Add task',
+        child: new Icon(Icons.add)
+      ),
     );
   }
 }
